d31f3ec359b8ac72e4d349f6fe880b79cadaa4a4
Teo <dianateodorav@gmail.com>
2025-03-15 00:46:06 +0200

    i am fkin tired man

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import Link from 'next/link';
 import Image from 'next/image';
 import {useEffect, useState} from "react";
+import hotLogo from './img/hot_wo_logo.png'
 
 export default function Home() {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -22,7 +22,7 @@ export default function Home() {
             {/* Header Section */}
             <header
                 className={`fixed w-full py-4 z-10 transition-colors ${isScrolled ? 'bg-gray-950 text-white' : 'bg-transparent text-white'}`}>
-
+                
                 <div className="container mx-auto flex justify-between items-center px-4">
                     <div className="flex items-center space-x-2">
                         <Image src="/favicon.ico" alt="Favicon" width={32} height={32}/>
@@ -47,14 +47,12 @@ export default function Home() {
 
             {/* Background with action buttons */}
             <div
-                className="relative h-screen bg-cover bg-center bg-[url(img/xikers_4_album_frontpage.png)]"
+                className="relative h-screen bg-cover bg-bottom bg-[url(img/xikers_4_album_frontpage.png)]"
             >
                 <div
-                    className="absolute inset-0  flex flex-col justify-center items-center space-y-4">
-                    <h1 className="text-4xl font-bold text-white">Welcome to the Landing Page</h1>
-                    <p className="text-lg text-white text-center">
-                        Discover amazing things with our platform.
-                    </p>
+                    className="absolute inset-0  flex flex-col justify-end items-center space-y-4 mb-16">
+
+                    <Image src={hotLogo} alt="Hot Logo" width={350} height={128}/>
                     <div className="flex space-x-4">
                         <button className="px-6 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-700">
                             Get Started
@@ -65,22 +63,6 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-
-
-            {/* Body Section */}
-            <main className="container mx-auto flex-grow px-4 py-8">
-                <h2 className="text-3xl font-bold text-center mb-6">Welcome to the Landing Page</h2>
-                <p className="text-lg text-center mb-6">
-                    This is the body section where you can add descriptions, images, or any other content you want.
-                </p>
-                <div className="text-center">
-                    <Link href="/x2kers">
-                        <button className="px-6 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-700">
-                            Go to X2Kers
-                        </button>
-                    </Link>
-                </div>
-            </main>
 
             {/* Footer Section */}
             <footer className="bg-gray-800 text-white py-4">
