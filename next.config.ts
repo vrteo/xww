241dc3835a2c224f10dcf8809c3fd2ff8924fd1e
Teo V <39007189+vrteo@users.noreply.github.com>
2025-06-06 16:14:12 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      // Add more hostname patterns as needed
+    ],
+    /* config options here */
+  },
+  
 };
+
 
 export default nextConfig;
